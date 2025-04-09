79969d695367f8efbf17226862d6a05a3c50cc35
Mark Bus <mark.bus.it@gmail.com>
2025-04-09 15:00:36 +0200

    minor refact

--- src/main/java/org/example/Parent.java
+++ src/main/java/org/example/Parent.java
@@ -1,22 +1,32 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class Parent extends Person {
 
-    private final List<Child> children = new ArrayList<>();
+    private List<Child> children;
 
     public Parent(String name, int age, String email) {
         super(name, age, email);
     }
 
-    public void addChild(Child child) {
-        children.add(child);
+    public void addChild(Child... children) {
+        Child[] childArray = Objects.requireNonNull(children);
+        if (this.children == null) {
+            this.children = new ArrayList<>();
+        }
+        this.children.addAll(List.of(childArray));
+    }
+
+    public void setChildren(List<Child> children) {
+        this.children = children;
     }
 
     public List<Child> getChildren() {
-        return children;
+        return children == null ? Collections.emptyList() : children;
     }
-    
+
 }
